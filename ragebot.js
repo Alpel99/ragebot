75da096415bba6f5dd6d1dc94e2ed56b2be0b3b7
cloud <cloud@discordbot.novalocal>
2020-12-28 14:38:48 +0100

    init

---
+++ ragebot.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+let url = "https://www.albion-online-data.com/api/v2/stats/prices/T4_BAG@3?locations=FortSterling&qualities=2";
+let settings = {method: "Get"};
+
+try {
+	var token = fs.readFileSync('token.txt', 'utf8');
+} catch {
+	//error
+}
+	
+client.login(token.toString().replace(/(\r\n|\n|\r)/gm, ""));//.catch(console.log);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  } 
+  if (msg.content === 'get') {
+	fetch(url, settings)
+	.then(res => res.json())
+	.then((json) => {
+		msg.reply('Price: ' + json[0].sell_price_min);
+    });
+	  
+	 // msg.reply('Price: ' +  getJson(url)[0].sell_price_min);
+  }
+});
+
+/*function getJson(url) {
+	 fetch(url, settings)
+        .then(res => res.json())
+        .then((json) => {
+		console.log("JSON: " + json[0]);
+		return json;
+    });
+}*/
